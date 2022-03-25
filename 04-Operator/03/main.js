8ef8cdaffe503b8a612d1875d35bdbc522042c8b
thianp <pthpatchara@gmail.com>
2022-03-26 01:26:39 +0700

    formatting changes

--- 04-Operator/03/main.js
+++ 04-Operator/03/main.js
@@ -1,11 +1,20 @@
-let a = undefined;
-let b = " ";
-let c = !b;
+// let a = undefined;
+// let b = " ";
+// let c = !b;
 
-let d = Boolean(a);
-let e = !!b;
-let f = Boolean(c);
+// let d = Boolean(a);
+// let e = !!b;
+// let f = Boolean(c);
 
-console.log(d); // false เพราะว่่าค่าบูลีนของ undefined คือ false
-console.log(e); // true string ไหนที่ไม่ empty จะนับเป็นค่าบูลีน true
-console.log(f); // false เพราะว่า ! ทำให้ตัวแปรนี้เป็นค่าบูลีนตรงข้ามกับค่าก่อนหน้า^
+// console.log(d); // false เพราะว่่าค่าบูลีนของ undefined คือ false
+// console.log(e); // true string ไหนที่ไม่ empty จะนับเป็นค่าบูลีน true
+// console.log(f); // false เพราะว่า ! ทำให้ตัวแปรนี้เป็นค่าบูลีนตรงข้ามกับค่าก่อนหน้า^
+
+let a = 10;
+let b = 20; 
+
+let sum = a + b;
+// let sumI = 0;
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+} 
